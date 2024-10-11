90098c8ed6598e6526676170887da7537e13167f
sneha datir <sneha.datir@d10x.io>
2024-10-11 09:55:16 +0530

    updating the urls

--- managementsystem/urls.py
+++ managementsystem/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("system/", include("bookms.urls")),
+    path('book/', include("bookms.urls")),
 ]
