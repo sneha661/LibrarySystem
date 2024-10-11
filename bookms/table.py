744050059d1133cdd0dc6526e87bec953cf1917f
sneha datir <sneha.datir@d10x.io>
2024-10-11 09:36:48 +0530

    add borrow button in BookTable

--- bookms/table.py
+++ bookms/table.py
@@ -1,24 +1,17 @@
 import django_tables2 as tables
-from django.utils.html import format_html
-from django.urls import reverse
 from .models import BookModel
 
 class BookTable(tables.Table):
-    Action = tables.Column(empty_values=())
+    Action = tables.TemplateColumn(
+        template_code='''
+            <a href="{% url 'bookms:book_update' record.id %}" class="btn btn-sm btn-primary">Update</a>
+            <a href="{% url 'bookms:book_delete' record.id %}" class="btn btn-sm btn-danger">Delete</a>
+            <a href="{% url 'bookms:book_borrow' record.id %}" class="btn btn-sm btn-warning">Borrow</a>
+        '''
+        
+    )
 
     class Meta:
         model = BookModel
         template_name = "django_tables2/bootstrap4.html"
-        fields = ("id", "name", "price", "author","version","publication_info","isbn","Action")
-
-    def render_Action(self, record):
-        # Include the app namespace in the reverse call
-        update_url = reverse('bookms:book_update', args=[record.id])
-        delete_url = reverse('bookms:book_delete', args=[record.id])
-
-        return format_html(
-            f'''
-            <a href="{update_url}" class="btn btn-sm btn-primary">Update</a> 
-            <a href="{delete_url}" class="btn btn-sm btn-danger">Delete</a>
-            '''
-        )
+        fields = ("id", "name", "price", "author", "version", "publication_info", "isbn", "Action")
